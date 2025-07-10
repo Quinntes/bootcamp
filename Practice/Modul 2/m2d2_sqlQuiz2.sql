dca8f2251e10ad3fe052673dc341fc1978e4970e
Lutfi <ahmadlutfi2263@gmail.com>
2025-07-10 10:47:20 +0700

    Add files via upload

---
+++ Practice/Modul 2/m2d2_sqlQuiz2.sql
@@ -0,0 +1,30 @@
+-- Evaluasi Kategori Negara Berdasarkan Populasi dan Harapan Hidup
+-- Database: world
+-- Tabel: country
+-- Konteks: Sebagai seorang Data Analyst di organisasi internasional, kamu diminta mengelompokkan negara-negara berdasarkan 
+--          populasi dan harapan hidup serta menilai kelompok negara mana yang memiliki stabilitas demografis yang lebih baik.
+
+/* Deskripsi
+Tampilkan data agregat berdasarkan kategori berikut:
+1. pop_group:
+	- 'Small' untuk populasi < 1 juta
+    - 'Medium' untuk populasi 1–50 juta
+    - 'Large' untuk populasi > 50 juta
+
+2. life_status:
+	- 'High' jika life expectancy ≥ 75
+    - 'Moderate' jika antara 60–74.9
+    - 'Low' jika < 60
+
+3. Hitung:
+	- Jumlah negara (total_negara)
+    - Rata-rata life expectancy (avg_life_exp)
+    - Rata-rata GNP (avg_gnp)
+
+4. Filter hanya kombinasi kategori yang punya rata-rata harapan hidup di atas 78 tahun
+
+5. Urutkan berdasarkan jumlah negara terbanyak */
+
+-- Your Code Here ...
+
+
